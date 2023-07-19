848c14ef6718384c3541674492185bce35ad0dac
Ben <b.e.eidson@gmail.com>
2023-07-18 21:22:06 -0400

    hover state added and runs much better. model smal

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,19 @@ import CanvasComponent from "@/components/CanvasComponent";
 import Link from "next/link";
 
 export default function Home() {
-  return(
-    <main className="flex justify-center w-screen h-screen ">
-      {/* <h1 className="text-white text-8xl z-10 mt-40">BEN EIDSON</h1> */}
-    <div className="absolute w-screen h-screen top-0 left-0">
-    <CanvasComponent/>
-    </div>
+  return (
+    <main className="flex w-screen h-screen justify-end items-end">
+      <div className="z-10 mb-20 mr-10 flex flex-col items-center">
+        <h1 className="text-white text-8xl mb-4">BEN EIDSON</h1>
+        <div className="flex justify-end w-full">
+          <Link href="/contact" className="z-20 text-4xl hover:bg-pink-400">
+            Contact Me
+          </Link>
+        </div>
+      </div>
+      <div className="absolute w-screen h-screen top-0 left-0">
+        <CanvasComponent />
+      </div>
     </main>
-  )
+  );
 }
--- components/CanvasComponent.jsx
+++ components/CanvasComponent.jsx
@@ -1,53 +1,85 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Text } from "@react-three/drei";
-import AsciiEffectComp from "@/components/AsciiEffectComp";
-import Ben from "@/components/Ben";
-import TextBen from '@/components/TextBen'
+import { useState, useEffect } from "react";
 
-import { useLoader } from '@react-three/fiber';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Text, Billboard, useCursor } from "@react-three/drei";
+
+import { useLoader } from "@react-three/fiber";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { TextureLoader } from "three";
 
+// <Text color={"purple"} position={[0,-5,-10]} rotation={[0, 0.0, 0.8]}>Coding</Text>
+//
 
+//////////////////////////
 
 export default function CanvasComponent() {
+  const [textData, setTextData] = useState([]);
+  const [hovered, setHovered] = useState([]);
+  useCursor(hovered); //controls cursor
 
-  function handleMusicClick(){
-    return 
+  const obj = useLoader(OBJLoader, "textModel/TextBen.obj");
+
+  useEffect(() => {
+    const fetchData = () => {
+      const modelGeometry = obj.children[0].geometry;
+      const positions = modelGeometry.attributes.position.array;
+      const existingArrays = new Set();
+      const textPos = [];
+
+      for (let i = 0; i < positions.length; i++) {
+        const x = positions[i * 3];
+        const y = positions[i * 3 + 1];
+        const z = positions[i * 3 + 2];
+
+        const newArray = [x, y, z];
+
+        if (!existingArrays.has(newArray.toString())) {
+          //get rid of duplicates
+          existingArrays.add(newArray.toString()); //add to set
+          const options = ["Ben", "Eidson", "Code", "Music", "Ben", "Eidson"];
+          const randomNum = Math.floor(Math.random() * options.length);
+          const type = options[randomNum];
+          textPos.push({ position: newArray, type: type, hovered: false }); //add to array
+        }
+
+        if (i > positions.length / 3 - 1) {
+          break;
+        }
+      }
+      setTextData(textPos);
+    };
+
+    fetchData();
+  }, []);
+
+  const handlePointerOver = (index) => {
+    setTextData((prevState) => {
+      const updatedData = [...prevState];
+      updatedData[index].hovered = true;
+      updatedData[index].type === "Code" || updatedData[index].type === "Music"
+        ? setHovered(true)
+        : setHovered(false);
+      return updatedData;
+    });
+  };
+
+  const handlePointerOut = (index) => {
+    setTextData((prevState) => {
+      const updatedData = [...prevState];
+      updatedData[index].hovered = false;
+      setHovered(false);
+      return updatedData;
+    });
+  };
+
+  function handleMusicClick() {
+    alert("music");
   }
-  function handleCodeClick(){
-    return 
+  function handleCodeClick() {
+    alert("code");
   }
-
-  const obj = useLoader(OBJLoader, 'textModel/TextBen.obj');
-  //const texture = useLoader(TextureLoader, 'Model/BEN_CLEAN.jpg');
-  const modelGeometry = obj.children[0].geometry;
-  const positions = modelGeometry.attributes.position.array;
-  const existingArrays = new Set()
-  const textPos = [];
-
-  for (let i = 0; i < positions.length; i++){ 
-    const x = positions[i*3];
-    const y = positions[i*3+1];
-    const z = positions[i*3+2];
-    
-    const newArray = [x,y,z]
-
-    if (!existingArrays.has(newArray.toString())){ //get rid of duplicates
-
-      existingArrays.add(newArray.toString()); //add to set
-      textPos.push(newArray); //add to array
-    }
-
-    console.log('bang')
-
-    if (i > positions.length / 3 - 1) {
-      break;
-    }
-  }
-console.log(textPos)
 
   return (
     <Canvas>
@@ -56,24 +88,25 @@ console.log(textPos)
       <spotLight position={[10, 0, 0.2]} angle={0.15} penumbra={1} />
 
       <OrbitControls />
-      {
-        textPos.map((array, i) =>{
-          const randomNum = Math.floor(Math.random() * 2 );
-          
-          return(
-          <Text position={array} key={i} fontSize={0.05} color={'black'}>{randomNum ==- 0 ? 'Code' : 'Music' }</Text>
-          )
-        })
-      }
+      {textData.map((text, i) => (
+        <Text
+          position={text.position}
+          key={i}
+          fontSize={0.05}
+          color={
+            text.hovered && (text.type === "Music" || text.type === "Code")
+              ? "red"
+              : "black"
+          }
+          onPointerOut={() => handlePointerOut(i)}
+          onPointerOver={() => handlePointerOver(i)}
+          onClick={handleMusicClick}
+        >
+          {text.type}
+        </Text>
+      ))}
     </Canvas>
   );
 }
 
-
-{/* <Text fontSize={0.1} color="white" position={[x, y, z]}>Test</Text> */}
-     {/* <Text color={"purple"} position={[0,-5,-10]} rotation={[0, 0.0, 0.8]}>Coding</Text>
-<Text color={"green"} position={[-8,-5,-9]} rotation={[0, 0.2, -0.8]}>Music</Text> */}
-{/* <Text color={"black"} position={[-4,11,-9]} rotation={[0, 0, 0]}>HI, I'M</Text>
-<mesh>
-</mesh>
-<Text color={"black"} position={[-4,10,-9]} rotation={[0, 0, 0]}>BEN EIDSON</Text> */}
+//billboard always faces user
