1a459d2b151103aeb8ae3d504e4c597279de234a
Ben Eidson <b.e.eidson@gmail.com>
2023-09-04 23:13:32 -0400

    basic youtube video hotfix using api

--- components/CodeShape.jsx
+++ components/CodeShape.jsx
@@ -1,24 +1,35 @@
 "use client";
 
-import React, { useRef, useEffect, useMemo } from "react";
+import { useRef, useEffect, useMemo } from "react";
 import { useThree } from "@react-three/fiber";
 import * as THREE from "three";
 import { Plane } from "@react-three/drei";
 import { Html } from "@react-three/drei";
 import Image from "next/image";
+import YouTube from "react-youtube";
+import VideoPlayer from "@/components/VideoPlayer";
 
 const cubeSize = 8; // Side length of the cube
 const halfSize = cubeSize / 2; // Half of the side length, used for positioning planes
 
-export default function CodeShape() {
+const opts = {
+  height: 300,
+  width: 300,
+};
 
+export default function CodeShape() {
   return (
     <>
       {/* Front face text offset fixes clipping*/}
       <Plane args={[cubeSize, cubeSize]} position={[0, 0, halfSize]}>
-      <meshStandardMaterial color="black" />
+        <meshStandardMaterial color="black" />
         <Html transform zIndexRange={[40, 0]}>
-        <Image src='/basicSideFace.png' alt="Picture of binSynth homepage" width={300} height={300}/> 
+          <Image
+            src="/basicSideFace.png"
+            alt="Picture of binSynth homepage"
+            width={300}
+            height={300}
+          />
         </Html>
       </Plane>
       {/* Back face */}
@@ -29,8 +40,13 @@ export default function CodeShape() {
       >
         <meshStandardMaterial color="black" />
         <Html transform zIndexRange={[40, 0]}>
-        <a href="https://binsynth.vercel.app/">
-          <Image src='/binsynth.png' alt="Picture of binSynth homepage" width={300} height={300}/> 
+          <a href="https://binsynth.vercel.app/">
+            <Image
+              src="/binsynth.png"
+              alt="Picture of binSynth homepage"
+              width={300}
+              height={300}
+            />
           </a>
         </Html>
       </Plane>
@@ -40,16 +56,10 @@ export default function CodeShape() {
         position={[0, halfSize, 0]}
         rotation={[-Math.PI / 2, 0, 0]}
       >
-        <meshStandardMaterial color="black"/>
+        <meshStandardMaterial color="black" />
 
         <Html transform zIndexRange={[40, 0]}>
-          <iframe
-            src="https://www.youtube.com/embed/AIMB2VJclkQ"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            className="w-48 h-48 md:w-72 md:h-72"
-          ></iframe>
+          <VideoPlayer videoID={"AIMB2VJclkQ"} />
         </Html>
       </Plane>
       {/* Bottom face */}
@@ -60,15 +70,8 @@ export default function CodeShape() {
       >
         <meshStandardMaterial color="black" />
 
-
         <Html transform zIndexRange={[40, 0]}>
-          <iframe
-            src="https://www.youtube.com/embed/ZwfgKpYsR80" //this is the don paul
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            className="w-48 h-48 md:w-72 md:h-72"
-          ></iframe>
+          <VideoPlayer videoID={"ZwfgKpYsR80"} />
         </Html>
       </Plane>
       {/* Right face */}
@@ -79,8 +82,7 @@ export default function CodeShape() {
       >
         <meshStandardMaterial color="black" />
         <Html transform zIndexRange={[40, 0]}>
-        {/* <Image src='/blackSide.png' alt="side of the cube" width={300} height={300}/>  */}
-
+          {/* <Image src='/blackSide.png' alt="side of the cube" width={300} height={300}/>  */}
         </Html>
       </Plane>
       {/* Left face */}
@@ -91,12 +93,32 @@ export default function CodeShape() {
       >
         <meshStandardMaterial color="black" />
         <Html transform zIndexRange={[40, 0]}>
-        {/* <Image src='/blackSide.png' alt="side of the cube" width={300} height={300} className="absolute"/>  */}
-        <Image src='/Website.png' alt="Picture of this website" width={300} height={300}/> 
+          {/* <Image src='/blackSide.png' alt="side of the cube" width={300} height={300} className="absolute"/>  */}
+          <Image
+            src="/Website.png"
+            alt="Picture of this website"
+            width={300}
+            height={300}
+          />
         </Html>
-
       </Plane>
-      </>
+    </>
   );
 }
 
+// use the video api?
+
+// <Html transform zIndexRange={[40, 0]}>
+// <div className="flex items-center justify-center">
+// <button className="">iframe bs fix</button>
+// <iframe
+// width={300}
+// height={300}
+//   src="https://www.youtube.com/embed/AIMB2VJclkQ"
+//   title="YouTube video player"
+//   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//   allowFullScreen
+//   className="touch-none pointer-events-none absolute"
+// ></iframe>
+// </div>
+// </Html>
---
+++ components/VideoPlayer.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useRef, useState } from "react";
+import YouTube from "react-youtube";
+
+const VideoPlayer = ({videoID}) => {
+  const playerRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
+
+  const onReady = (event) => {
+    // Store the player object for use later
+    playerRef.current = event.target;
+  };
+
+  const onPlay = () => {
+    setIsPlaying(true);
+  };
+
+  const onPause = () => {
+    setIsPlaying(false);
+    setIsStarted(true)
+  };
+
+  const playVideo = () => {
+    if (playerRef.current) {
+      playerRef.current.playVideo();
+    }
+  };
+
+  const opts = {
+    height: "300",
+    width: "300",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const pointerEventsClass = isPlaying ? 'pointer-events-auto' : 'pointer-events-none';
+  const zButton = isStarted ? 'z-40 bg-red-500 rounded-lg p-2' : ''
+
+  return (
+    <div className="flex items-center justify-center">
+      {!isPlaying && <button onClick={playVideo} className={`${zButton}`}>Resume</button>}
+      <YouTube
+        videoId={videoID}
+        opts={opts}
+        onReady={onReady}
+        onPlay={onPlay}
+        onPause={onPause}
+        className={`pointer-events-none touch-none absolute ${pointerEventsClass}`}
+      />
+    </div>
+  );
+};
+
+export default VideoPlayer;
