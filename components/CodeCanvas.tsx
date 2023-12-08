92019e71d6b80b8c1a6d215e93bf44147e721bc2
Ben Eidson <b.e.eidson@gmail.com>
2023-12-08 01:46:29 -0500

    keyboard control of code page

--- components/CodeCanvas.tsx
+++ components/CodeCanvas.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/no-unknown-property */
 "use client";
 
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import CodeShape from "@/components/CodeShape";
 import { useDrag } from "@use-gesture/react";
 import * as THREE from "three";
 import codingData from "@/data/coding";
 import CodeModal from "@/components/CodeModal";
+import { A11y } from "@react-three/a11y";
 
 const cameraPosition = [0, 0, 5]; // for static camera position
 
@@ -23,6 +24,37 @@ export default function CodeCanvas() {
 
   const cameraPositionVector = new THREE.Vector3(...cameraPosition);
 
+  useEffect(() => {
+    const handleKeyDown = (event: any) => {
+      if (groupRef.current != null) {
+        switch (event.key) {
+          case "ArrowUp":
+            groupRef.current.rotation.x += Math.PI / 2;
+            break;
+          case "ArrowDown":
+            groupRef.current.rotation.x -= Math.PI / 2;
+            break;
+          case "ArrowLeft":
+            groupRef.current.rotation.y -= Math.PI / 2;
+            break;
+          case "ArrowRight":
+            groupRef.current.rotation.y += Math.PI / 2;
+            break;
+          default:
+            break;
+        }
+        get_closest_face();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   const bind = useDrag((state) => {
     state.event.preventDefault();
     state.event.stopPropagation();
@@ -50,28 +82,30 @@ export default function CodeCanvas() {
       if (groupRef.current != null) {
         groupRef.current.rotation.x += deltaY * 0.01;
         groupRef.current.rotation.y += deltaX * 0.01;
-
-        const worldPosition = new THREE.Vector3();
-        let minDistance = Infinity; // Initialize to a large value
-        let closestChildIndex = -1; // Initialize to an invalid index
-
-        groupRef.current.children.forEach((child, index) => {
-          child.getWorldPosition(worldPosition);
-          const distance = cameraPositionVector.distanceTo(worldPosition);
-
-          if (distance < minDistance) {
-            minDistance = distance;
-            closestChildIndex = index;
-          }
-        });
-        // closestFaceBABY
-        setClosestFace(closestChildIndex);
+        get_closest_face();
       }
-
       // Update the last position
       setLastPosition({ x, y });
     }
   });
+  function get_closest_face() {
+    const worldPosition = new THREE.Vector3();
+    let minDistance = Infinity; // Initialize to a large value
+    let closestChildIndex = -1; // Initialize to an invalid index
+    if (groupRef.current != null) {
+      groupRef.current.children.forEach((child, index) => {
+        child.getWorldPosition(worldPosition);
+        const distance = cameraPositionVector.distanceTo(worldPosition);
+
+        if (distance < minDistance) {
+          minDistance = distance;
+          closestChildIndex = index;
+        }
+      });
+      // closestFaceBABY
+      setClosestFace(closestChildIndex);
+    }
+  }
 
   return (
     <div {...bind()} className="touch-none h-[calc(100dvh)]">
@@ -79,7 +113,12 @@ export default function CodeCanvas() {
         <color attach="background" args={["pink"]} />
         <ambientLight intensity={1} color={"white"} />
         <group ref={groupRef} position={[0, 1, -8]}>
-          <CodeShape />
+          <A11y
+            role="content"
+            description="Cube that holds all of my projects. Use mouse or arrow keys to see different projects"
+          >
+            <CodeShape />
+          </A11y>
         </group>
       </Canvas>
       <CodeModal side={codingData[closestFace]} />
