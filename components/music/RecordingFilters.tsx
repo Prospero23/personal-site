45d69b589a63392d83cff3026e605d4f6b37a365
Ben Eidson <b.e.eidson@gmail.com>
2025-11-15 14:23:20 -0500

    Improve filter styles

--- components/music/RecordingFilters.tsx
+++ components/music/RecordingFilters.tsx
@@ -12,46 +12,47 @@ export type Instrument = "sax" | "laptop"
 export default function RecordingFilters({currentRecordings, setCurrentRecordings, currentInstrument, setCurrentInstrument}: RecordingKindsProps){
 
     return(
-    <div className="absolute top-16 md:right-20 md:top-32 z-10 flex flex-col gap-2 pointer-events-none shadow-lg">
-    {/* Top row */}
-    <div className="pointer-events-auto inline-flex items-center gap-1 px-2 py-1 text-xs sm:text-sm md:text-base">
-        <FilterButton
-        isActive={currentRecordings === "highlights"}
-        activeClassName="text-green-500"
-        onClick={() => setCurrentRecordings("highlights")}>
-        Highlights
-        </FilterButton>
-        <FilterButton
-        isActive={currentRecordings === "audio"}
-        activeClassName="text-orange-500"
-        onClick={() => setCurrentRecordings("audio")}>
-        Audio
-        </FilterButton>
-        <FilterButton
-        isActive={currentRecordings === "video"}
-        activeClassName="text-blue-500"
-        onClick={() => setCurrentRecordings("video")}>
-        Videos
-        </FilterButton>
-    </div>
-
-    {/* Bottom row */}
-    <div className="pointer-events-auto inline-flex justify-around items-center text-xs sm:text-sm md:text-base">
-        <FilterButton
-        isActive={currentInstrument == "sax"}
-        activeClassName="text-purple-600"
-        onClick={() => setCurrentInstrument("sax")}
-        >
-        Sax
-        </FilterButton>
-        <FilterButton
-        isActive={currentInstrument == "laptop"}
-        activeClassName="text-red-600"
-        onClick={() => setCurrentInstrument("laptop")}
-        >
-        Laptop
-        </FilterButton>
-    </div>
+    <div className="pointer-events-none absolute inset-x-0 top-20 md:top-24 z-10 flex justify-center text-white">
+      <div className="flex flex-col shadow-lg">
+        {/* Top row */}
+        <div className="pointer-events-auto inline-flex items-center gap-1 px-2 py-1 text-sm sm:text-base md:text-lg">
+            <FilterButton
+            isActive={currentRecordings === "highlights"}
+            activeClassName="text-green-500"
+            onClick={() => setCurrentRecordings("highlights")}>
+            Highlights
+            </FilterButton>
+            <FilterButton
+            isActive={currentRecordings === "audio"}
+            activeClassName="text-orange-500"
+            onClick={() => setCurrentRecordings("audio")}>
+            Audio
+            </FilterButton>
+            <FilterButton
+            isActive={currentRecordings === "video"}
+            activeClassName="text-blue-500"
+            onClick={() => setCurrentRecordings("video")}>
+            Videos
+            </FilterButton>
+        </div>
+        {/* Bottom row */}
+        <div className="pointer-events-auto inline-flex justify-around items-center text-sm sm:text-base md:text-lg">
+            <FilterButton
+            isActive={currentInstrument == "sax"}
+            activeClassName="text-purple-600"
+            onClick={() => setCurrentInstrument("sax")}
+            >
+            Sax
+            </FilterButton>
+            <FilterButton
+            isActive={currentInstrument == "laptop"}
+            activeClassName="text-red-600"
+            onClick={() => setCurrentInstrument("laptop")}
+            >
+            Laptop
+            </FilterButton>
+        </div>
+      </div>
     </div>
     )
 }
@@ -75,7 +76,7 @@ function FilterButton({
   return (
     <button
       className={`pointer-events-auto p-1 hover:underline ${
-        isActive ? activeClassName : inactiveClassName
+        isActive ? `${activeClassName} underline` : inactiveClassName
       }`}
       {...props}
     >
