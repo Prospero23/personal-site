dabdb18f2aefa9241b6f06bba3f0811358f9d6ff
Ben Eidson <b.e.eidson@gmail.com>
2025-11-27 10:40:39 -0500

    Make view code optional

--- components/code/modal/ProjectHeader.tsx
+++ components/code/modal/ProjectHeader.tsx
@@ -1,7 +1,7 @@
 interface ProjectHeaderProps {
   dates: string;
   summary: string;
-  codeUrl: string;
+  codeUrl?: string;
 }
 
 export default function ProjectHeader({
@@ -18,16 +18,18 @@ export default function ProjectHeader({
         <p className="mt-1 text-sm text-zinc-800">{summary}</p>
       </div>
 
-      <div className="flex items-center gap-3">
-        <a
-          href={codeUrl}
-          target="_blank"
-          rel="noreferrer"
-          className="rounded-full border border-zinc-700 px-3 py-1 text-xs font-semibold uppercase tracking-wide text-zinc-800 hover:bg-zinc-800 hover:text-customPink transition text-center"
-        >
-          View code
-        </a>
-      </div>
+      {codeUrl && (
+        <div className="flex items-center gap-3">
+          <a
+            href={codeUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="rounded-full border border-zinc-700 px-3 py-1 text-xs font-semibold uppercase tracking-wide text-zinc-800 hover:bg-zinc-800 hover:text-customPink transition text-center"
+          >
+            View code
+          </a>
+        </div>
+      )}
     </div>
   );
 }
