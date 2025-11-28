f23be08c4c04ff229927b725c3b7e672d3c7552e
Ben Eidson <b.e.eidson@gmail.com>
2025-11-28 00:20:04 -0500

    Music filters proper positioning with short screen

--- components/music/RecordingFilters.tsx
+++ components/music/RecordingFilters.tsx
@@ -21,41 +21,41 @@ export default function RecordingFilters({
   setCurrentInstrument,
 }: RecordingKindsProps) {
   return (
-    <div
-      className={`absolute inset-x-0 top-20 md:top-24 z-10 flex justify-center short-screen:justify-start`}
-    >
-      <div className="backdrop-blur-md bg-black/30 rounded-full px-4 py-2 flex flex-col gap-1 shadow-md pointer-events-auto">
-        {/* Category */}
-        <div className="flex gap-2 justify-center">
-          <FilterButton
-            label="Highlights"
-            active={currentRecordings === "highlights"}
-            onClick={() => setCurrentRecordings("highlights")}
-          />
-          <FilterButton
-            label="Audio"
-            active={currentRecordings === "audio"}
-            onClick={() => setCurrentRecordings("audio")}
-          />
-          <FilterButton
-            label="Videos"
-            active={currentRecordings === "video"}
-            onClick={() => setCurrentRecordings("video")}
-          />
-        </div>
+    <div className={`absolute inset-x-0 top-20 md:top-24 z-10`}>
+      <div className="mx-auto w-full lg:max-w-7xl flex justify-center short-screen:justify-start">
+        <div className="backdrop-blur-md bg-black/30 rounded-full px-4 py-2 flex flex-col gap-1 shadow-md pointer-events-auto">
+          {/* Category */}
+          <div className="flex gap-2 justify-center">
+            <FilterButton
+              label="Highlights"
+              active={currentRecordings === "highlights"}
+              onClick={() => setCurrentRecordings("highlights")}
+            />
+            <FilterButton
+              label="Audio"
+              active={currentRecordings === "audio"}
+              onClick={() => setCurrentRecordings("audio")}
+            />
+            <FilterButton
+              label="Videos"
+              active={currentRecordings === "video"}
+              onClick={() => setCurrentRecordings("video")}
+            />
+          </div>
 
-        {/* Instrument */}
-        <div className="flex gap-2 justify-center">
-          <FilterButton
-            label="Sax"
-            active={currentInstrument === "sax"}
-            onClick={() => setCurrentInstrument("sax")}
-          />
-          <FilterButton
-            label="Laptop"
-            active={currentInstrument === "laptop"}
-            onClick={() => setCurrentInstrument("laptop")}
-          />
+          {/* Instrument */}
+          <div className="flex gap-2 justify-center">
+            <FilterButton
+              label="Sax"
+              active={currentInstrument === "sax"}
+              onClick={() => setCurrentInstrument("sax")}
+            />
+            <FilterButton
+              label="Laptop"
+              active={currentInstrument === "laptop"}
+              onClick={() => setCurrentInstrument("laptop")}
+            />
+          </div>
         </div>
       </div>
     </div>
